ab3d16d351b480acff25a7f76f2ae36139f82e35
Jacob <encody@users.noreply.github.com>
2023-12-27 20:59:46 -0800

    feat: load individual files

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use std::path::{Path, PathBuf};
 use clap::Parser;
 use stoat::note::{Note, NoteId};
 
+static IGNORE_FILES_WITH_LEADING: &[char] = &['.', '_'];
+static NOTES_FILE_EXTENSION: &str = "md"; // not including dot
+
 #[derive(Debug, Parser)]
 struct Args {
     #[clap(short, long)]
@@ -14,38 +17,41 @@ fn load_directory(path: &Path) -> Vec<Note> {
 
     files_in_index
         .flatten()
-        .filter_map(|file| {
-            let file_name = file.file_name().into_string().ok()?;
-
-            // ignore files with leading . or _
-            // ignore non-markdown files
-            if file_name.starts_with(['.', '_']) || !file_name.ends_with(".md") {
-                return None;
-            }
-
-            // .metadata() traverses symlinks
-            let metadata = file.metadata().ok()?;
-
-            if !metadata.is_file() {
-                return None;
-            }
-
-            println!("{}", file.path().display());
-
-            let contents = std::fs::read_to_string(file.path()).ok()?;
-
-            println!("{}", contents);
-            let id: NoteId = file_name[..file_name.len() - 3].into();
-
-            Note::create(id, &contents).ok()
-        })
+        .filter_map(|entry| try_load_file(&entry.path()))
         .collect()
+}
+
+fn try_load_file(path: &Path) -> Option<Note> {
+    // .metadata() traverses symlinks
+    let metadata = path.metadata().ok()?;
+
+    if !metadata.is_file() {
+        return None;
+    }
+
+    let file_name = path.file_name()?.to_str()?;
+
+    let extension = path.extension()?;
+
+    if file_name.starts_with(IGNORE_FILES_WITH_LEADING) || extension != NOTES_FILE_EXTENSION {
+        return None;
+    }
+
+    let contents = std::fs::read_to_string(path).ok()?;
+
+    let id: NoteId = file_name[..file_name.len() - (NOTES_FILE_EXTENSION.len() + 1)].into();
+
+    Note::create(id, &contents).ok()
 }
 
 fn main() {
     let args = Args::parse();
 
-    let notes = load_directory(&args.index);
+    let notes = if args.index.is_dir() {
+        load_directory(&args.index)
+    } else {
+        vec![try_load_file(&args.index).expect("Could not load file")]
+    };
 
     println!("Notes:\n\n{notes:#?}");
 
